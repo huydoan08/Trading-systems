204fd6383b8b71c3405cdce81f042537e02d2f66
huy.nguyen <huy.nguyen8@sotatek.com>
2025-03-20 16:53:34 +0700

    fd

--- src/app/(demo)/strategy/page.tsx
+++ src/app/(demo)/strategy/page.tsx
@@ -1,30 +1,33 @@
 "use client";
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { useSidebar } from "@/hooks/use-sidebar";
 import { useStore } from "@/hooks/use-store";
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "@/firebaseConfig";
+import { motion } from "framer-motion";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
-const multiTime = [
-  "Cập nhật tình hình thị trường ngày 20-03-2025:",
-  "Khung 1M => Lưỡng lự, đo fibo vẫn giữ cấu trúc sóng tăng.",
-  "Khung 1W => Giảm, bắt đầu có dấu hiệu điều chỉnh.",
-  "Khung D1 => Sideway, bắt đầu cắt lên trên 2 DTB.",
-  "Khung H4 => Tăng, RSI đang mở rộng, mới chạm vùng 70 đã có dấu hiệu điều chỉnh",
-  "Khung H1 => RSI chưa chạm vùng 80 thì có dấu hiệu điều chỉnh, lực tăng không thực sự mạnh mẽ, cần chú ý quan sát tiếp.."
-];
-const planningH4 = [
-  "Chiến lược ngắn hạn => Vào hàng BTC, quảng lý SL đáy 82K, quản lý theo cấu trúc sóng H4."
-];
-const planningW = [
-  "Chiến lược dài hạn => Chưa có plan phân bổ vốn cho dài hạn."
+const PTKT = [
+  {
+    title: "Cập nhật tình hình thị trường chart BTC ngày 20-03-2025:",
+    content: [
+      "Khung 1M => RSI lưỡng lự, đo fibo vẫn giữ cấu trúc sóng tăng.",
+      "Khung 1W => RSI giảm, nhưng có dấu hiệu bó lại.",
+      "Khung D1 => RSI sideway, nhưng bắt đầu cắt lên trên 2 đường TB.",
+      "Khung H4 => RSI tăng và đang mở rộng, mới chạm vùng 70 đã có dấu hiệu điều chỉnh, lực tăng chưa thực sự mạnh mẽ.",
+      "Khung H1 => RSI tăng, chưa chạm vùng 80 thì có dấu hiệu điều chỉnh, lực tăng không thực sự mạnh mẽ, cần chú ý quan sát tiếp.",
+      "CHIẾN LƯỢC NGẮN HẠN => LƯỚT SÓNG NGẮN H4, QUẢN LÝ LỆNH TẠI VÙNG 82K.",
+      "CHIẾN LƯỢC DÀI HẠN => KHUNG 1W ĐÃ PHÂN KÌ NÊN THOÁT VỐN LỚN, CHỜ SETUP MỚI."
+    ]
+  }
 ];
 
 export default function StrategyPage() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
   const router = useRouter();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -39,44 +42,48 @@ export default function StrategyPage() {
   if (!sidebar) return null;
   return (
     <ContentLayout title="Chiến lược giao dịch">
-      <Card className="max-h-[67.5vh] overflow-auto shadow-lg border border-black-200 dark:border-black-700">
-        <CardContent className="p-6 space-y-4">
-          <div className="font-bold text-lg text-black-800 dark:text-white">
-            Quan sát đa khung thời gian để nhìn thấy được bức tranh tổng thể của
-            toàn bộ thị trường:
+      <Card className="max-h-[67.5vh] overflow-auto shadow-lg border border-black-200 dark:border-black-700 mb-2">
+        <CardHeader>
+          <div className="font-bold text-2xl text-black-800 dark:text-white">
+            View về đa khung thời gian để nhìn thấy được bức tranh tổng thể thị trường:
           </div>
-          <div className="space-y-2">
-            {multiTime.map((item, idx) => (
-              <div key={idx} className="flex items-start space-x-2 ">
-                <span className="text-black-600 dark:text-white">-</span>
-                <Label className="text-black-700 font-semibold dark:text-white">
-                  {item}
-                </Label>
-              </div>
-            ))}
-            {planningH4.map((item, idx) => (
-              <div key={idx} className="flex items-start space-x-2 ">
-                <span className="text-black-600 text-green-600 dark:text-green-400">
-                  -
-                </span>
-                <Label className="text-black-700 text-green-600 font-semibold dark:text-green-400">
-                  {item}
-                </Label>
-              </div>
-            ))}
-            {planningW.map((item, idx) => (
-              <div key={idx} className="flex items-start space-x-2 ">
-                <span className="text-black-600 text-blue-600 dark:text-blue-400">
-                  -
-                </span>
-                <Label className="text-black-700 text-blue-600 font-semibold dark:text-blue-400">
-                  {item}
-                </Label>
-              </div>
-            ))}
-          </div>
-        </CardContent>
+        </CardHeader>
       </Card>
+      {PTKT.map((item, index) => (
+        <Card
+          key={index}
+          className="max-h-[67.5vh] overflow-hidden shadow-lg border border-black-200 dark:border-black-700 mt-1 cursor-pointer"
+          onClick={() => setOpenIndex(openIndex === index ? null : index)}
+        >
+          <div className="flex justify-between items-center p-6">
+            <span className="font-bold text-lg text-black-800 dark:text-white">
+              {item.title}
+            </span>
+            {openIndex === index ? <ChevronUp /> : <ChevronDown />}
+          </div>
+          <motion.div
+            initial={{ height: 0, opacity: 0 }}
+            animate={
+              openIndex === index
+                ? { height: "auto", opacity: 1 }
+                : { height: 0, opacity: 0 }
+            }
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="overflow-hidden"
+          >
+            <CardContent className="p-6 space-y-2 border-t text-black-600 dark:text-white">
+              {item.content.map((item, idx) => (
+                <div key={idx} className="flex items-start space-x-2">
+                  <span className="text-black-600 dark:text-white">-</span>
+                  <Label className="text-black-700 font-semibold dark:text-white">
+                    {item}
+                  </Label>
+                </div>
+              ))}
+            </CardContent>
+          </motion.div>
+        </Card>
+      ))}
     </ContentLayout>
   );
 }
