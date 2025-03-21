181051a58147d4b71be66d6226222faaa7a5f7fc
HuyDoan <doanvanhuy268@gmail.com>
2025-03-21 22:37:15 +0700

    fd

--- src/app/(demo)/strategy/page.tsx
+++ src/app/(demo)/strategy/page.tsx
@@ -85,7 +85,7 @@ export default function StrategyPage() {
             transition={{ duration: 0.3, ease: "easeInOut" }}
             className="overflow-hidden"
           >
-            <CardContent className="p-6 space-y-2 border-t text-black-600 dark:text-white">
+            <CardContent className="p-6 space-y-2 border-t text-black-600 dark:text-white max-h-[30vh] overflow-auto sm:max-h-none sm:overflow-visible">
               {item.content.map((item, idx) => (
                 <div key={idx} className="flex items-start space-x-2">
                   <span className="text-black-600 dark:text-white">-</span>
--- src/app/(demo)/technical-question/page.tsx
+++ src/app/(demo)/technical-question/page.tsx
@@ -89,7 +89,7 @@ export default function RSIPage() {
             transition={{ duration: 0.3, ease: "easeInOut" }}
             className="overflow-hidden"
           >
-            <CardContent className="p-6 space-y-2 border-t text-black-600 dark:text-white">
+            <CardContent className="p-6 space-y-2 border-t text-black-600 dark:text-white max-h-[30vh] overflow-auto sm:max-h-none sm:overflow-visible" >
               {faq.content.map((item, idx) => (
                 <div key={idx} className="flex items-start space-x-2">
                   <span className="text-black-600 dark:text-white">-</span>
