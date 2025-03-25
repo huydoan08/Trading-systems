c0eb8e79f6983cd83759de3c772a42997f45a1c8
huy.nguyen <huy.nguyen8@sotatek.com>
2025-03-25 12:08:22 +0700

    fd

--- src/app/(demo)/technical-question/page.tsx
+++ src/app/(demo)/technical-question/page.tsx
@@ -7,44 +7,7 @@ import { useStore } from "@/hooks/use-store";
 import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { ExpandableCard } from "@/app/component/ExpandableCard/ExpandableCard";
-
-const faqs = [
-  {
-    title: "1.Tín hiệu phân kì là gì ?",
-    content: [
-      "Tức là giá thì tăng mà RSI thì tạo đỉnh thấp dần ( giải thích điều này là khi mà giá tăng mạnh nhưng mà lúc này RSI đi xuống tạo đỉnh thấp dần, chứng tỏ lực mua đã suy yếu, lực bán đã tham gia vào nhiều, thì cần lập tức bỏ chạy.",
-      "Lưu ý: Có dấu hiệu phân kì cũng chưa chắc là giá đã giảm, mà giá nó có thể giảm xíu xong nó tăng tiếp rất mạnh. Việc của mình là thấy có dấu hiệu phân kì thì mình biết rằng giá có khả năng điều chỉnh, còn điều chỉnh xong nó có tăng tiếp hay không thì câu chuyện đó tính sau .Nếu thấy RSI điều chỉnh xong và đo fibo thấy vẫn giữ giá tốt thì lại tiếp tục tham gia mua vào"
-    ]
-  },
-  {
-    title:
-      "2.RSI ở các khung thời gian khác nhau thì cho các tín hiệu chỉ báo khác nhau như vậy có phải đồng thuận không ?",
-    content: [
-      "Áp dụng lý thuyết của sóng Eliot thì trong con sóng tăng chính sẽ có những con sóng nhỏ, trong con sóng nhỏ sẽ có những con sóng nhỏ hơn, khung thời gian càng lớn thì cho thấy xu hướng chính càng rõ rệt.",
-      "Trong con sóng tăng cũng có thời điểm nó sẽ dừng lại điều chỉnh, sideway rồi mới tăng tiếp. Khi này nếu nhìn ở khung nhỏ hơn thì sẽ khá nhiễu loạn. Việc của mình là khi view khung nhỏ thấy nhiễu loạn thì sẽ bỏ qua và chuyển sang khung lớn hơn, có tín hiệu rõ ràng hơn để follow theo."
-    ]
-  },
-  {
-    title:
-      "3.Khi nào thì nên đặt lệnh dừng lỗ, khi nào thì không cần đặt lệnh dừng lỗ ?",
-    content: [
-      "Đặt lệnh dừng lỗ cần tuân thủ tuyệt đối trong một số trường hợp như sau: giao dịch ở khung H4 trở xuống, giao dịch phái sinh.",
-      "Lệnh dừng lỗ không cần thiết phải đặt nếu đầu tư trong khung thời gian 1D trở lên, thậm chí có thể áp dụng chiến lược mua DCA khi giá tiếp tục giảm, nhưng phải hiểu thật sự sâu về đầu tư, cũng như vốn phải thật sự sẵn sàng cho dài hạn."
-    ]
-  },
-  {
-    title: "4.Có nên giao dịch future hay không?",
-    content: [
-      "Đối với những người mới chưa có kĩ năng master về trading thì lời khuyên chân thành là không nên, làm sao mà giao dịch spot có được lợi nhuận ổn định trong một thời gian rất là dài rồi thì khi ấy mới cân nhắc tới việc đánh future."
-    ]
-  },
-  {
-    title: "5.Có nên đánh Altcoin hay không?",
-    content: [
-      "Altcoin nó khá là rủi ro nên nếu tham gia thì chỉ dành 1 số vốn nhỏ, và phải tìm hiểu thật kĩ lưỡng về dự án đó trước khi đưa ra quyết định đầu tư, ngoài ra cũng cần xác định xem chu kì thị trường đang ở giai đoạn nào."
-    ]
-  }
-];
+import { faqs } from "@/data/data";
 
 export default function RSIPage() {
   const sidebar = useStore(useSidebar, (x) => x);
--- src/data/data.ts
+++ src/data/data.ts
@@ -4,8 +4,7 @@ export const experience = [
   {
     title: "Ngày 25-03-2025",
     content: [
-      "Thực ra vẫn còn nhiều bạn có những suy nghĩ rất ngây thơ...đại loại như là: Túm lại bây giờ mua coin nào, cổ phiếu nào...chỉ cần cho cái tên.\n\nNhưng thực tế...đầu tư đâu đơn giản thế. Nếu các bạn quan sát bên ngoài cộng đồng thì sẽ thấy là với tư duy như vậy người ta thua lỗ, bị chia tài khoản và cháy tài khoản rất nhiều với tư duy như vậy.\n\nNên khi mà mình hiểu ra điều này mình không chia sẻ kèo cọt, hạn chế chia sẻ nhận định. Không phải mình sợ hay ích kỷ gì, mà là nếu làm điều đó cho bạn thì đích đến bạn cũng sẽ thua mà thôi.\n\nThay đổi tư duy trước, học tập và đào sâu...\n\nSẽ có giai đoạn thuận lợi, sẽ có giai đoạn khó khăn...nhưng mình tin là bạn sẽ học được rất nhiều trong những giai đoạn khó khăn, còn mà mua đâu thắng đó thì chỉ làm cho bạn kiêu ngạo, tự cao...và chắc chắn là sau đó sẽ sấp mặt.\n\nNếu bạn có sự mong cầu học tập, hãy đặt mục tiêu học tập...thay vì mong cầu kiếm được tiền. Bạn sẽ làm rất tốt...\n\nCòn nếu thực sự mục tiêu của bạn là kiếm tiền vội vàng, bạn sẽ chẳng học được gì nhiều...đặc biệt khi mà có những thứ không thuận lợi đến thì trong bạn luôn là sự trách móc, oán giận...thay vì nhìn ra những bài học.\n\nHãy quan sát ở bên ngoài cộng đồng mà xem...họ đi chửi người này, chửi người kia...đã từng chia sẻ hay chỉ họ mua coin...nhưng người quyết định cuối cùng vẫn là bản thân họ, họ là người click và bấm nút mua bán...xong rồi đi chửi người khác.\n\nThắng thì họ cho rằng họ giỏi, thua thì họ đi đổ lỗi cho người khác...đám đông luôn vậy...khi hiểu ra điều này thì mình hầu như không đi hóng hớt kèo cọt, mà chỉ tập trung vào bản thân, và cũng không share kèo ra ngoài vì việc làm đó không có giá trị gì, vô ích và luôn rình rập \"hoạ\" cho bản thân.\n\nNgười ta không biết kỹ năng xử lý, đúng lần 1, đúng lần 2 nhưng chỉ cần 1 lần sai...họ sẽ mất hết và trách bạn...\n\nMình đã trải qua rồi, nên đó là lý do tại sao ở đây mình chỉ dừng lại ở mức độ chia sẻ kiến thức, kỹ năng xử lý....chứ không nói về các hành động mua bán. Vì nếu bạn không hiểu...vô tình mình làm hại chính bạn.\n\nCác bạn cần phải có chiều sâu, học tập và dành thời gian suy ngẫm...đừng vội vàng quyết định một thứ gì khi mình chưa hiểu rõ...\n\nĐích đến của trading và đầu tư là phục vụ cho cuộc sống. Hãy làm sao đầu tư một cách thoải mái và bình an...dù kết quả thế nào...vẫn thoải mái, vẫn sống tốt...đó mới là cách đầu tư đúng đắn.\n\n",
-      
+      'Thực ra vẫn còn nhiều bạn có những suy nghĩ rất ngây thơ...đại loại như là: Túm lại bây giờ mua coin nào, cổ phiếu nào...chỉ cần cho cái tên.\n\nNhưng thực tế...đầu tư đâu đơn giản thế. Nếu các bạn quan sát bên ngoài cộng đồng thì sẽ thấy là với tư duy như vậy người ta thua lỗ, bị chia tài khoản và cháy tài khoản rất nhiều với tư duy như vậy.\n\nNên khi mà mình hiểu ra điều này mình không chia sẻ kèo cọt, hạn chế chia sẻ nhận định. Không phải mình sợ hay ích kỷ gì, mà là nếu làm điều đó cho bạn thì đích đến bạn cũng sẽ thua mà thôi.\n\nThay đổi tư duy trước, học tập và đào sâu...\n\nSẽ có giai đoạn thuận lợi, sẽ có giai đoạn khó khăn...nhưng mình tin là bạn sẽ học được rất nhiều trong những giai đoạn khó khăn, còn mà mua đâu thắng đó thì chỉ làm cho bạn kiêu ngạo, tự cao...và chắc chắn là sau đó sẽ sấp mặt.\n\nNếu bạn có sự mong cầu học tập, hãy đặt mục tiêu học tập...thay vì mong cầu kiếm được tiền. Bạn sẽ làm rất tốt...\n\nCòn nếu thực sự mục tiêu của bạn là kiếm tiền vội vàng, bạn sẽ chẳng học được gì nhiều...đặc biệt khi mà có những thứ không thuận lợi đến thì trong bạn luôn là sự trách móc, oán giận...thay vì nhìn ra những bài học.\n\nHãy quan sát ở bên ngoài cộng đồng mà xem...họ đi chửi người này, chửi người kia...đã từng chia sẻ hay chỉ họ mua coin...nhưng người quyết định cuối cùng vẫn là bản thân họ, họ là người click và bấm nút mua bán...xong rồi đi chửi người khác.\n\nThắng thì họ cho rằng họ giỏi, thua thì họ đi đổ lỗi cho người khác...đám đông luôn vậy...khi hiểu ra điều này thì mình hầu như không đi hóng hớt kèo cọt, mà chỉ tập trung vào bản thân, và cũng không share kèo ra ngoài vì việc làm đó không có giá trị gì, vô ích và luôn rình rập "hoạ" cho bản thân.\n\nNgười ta không biết kỹ năng xử lý, đúng lần 1, đúng lần 2 nhưng chỉ cần 1 lần sai...họ sẽ mất hết và trách bạn...\n\nMình đã trải qua rồi, nên đó là lý do tại sao ở đây mình chỉ dừng lại ở mức độ chia sẻ kiến thức, kỹ năng xử lý....chứ không nói về các hành động mua bán. Vì nếu bạn không hiểu...vô tình mình làm hại chính bạn.\n\nCác bạn cần phải có chiều sâu, học tập và dành thời gian suy ngẫm...đừng vội vàng quyết định một thứ gì khi mình chưa hiểu rõ...\n\nĐích đến của trading và đầu tư là phục vụ cho cuộc sống. Hãy làm sao đầu tư một cách thoải mái và bình an...dù kết quả thế nào...vẫn thoải mái, vẫn sống tốt...đó mới là cách đầu tư đúng đắn.\n\n'
     ]
   },
   {
@@ -144,4 +143,56 @@ export const importantNotes = [
   "Những dấu hiệu thì luôn luôn chỉ mang tính xác suất không chắc chắn một điều gì cả.",
   "Điều quan trọng nhất là tập trung vào kĩ năng xử lý chứ không phải dự đoán giá.",
   "Chỉ làm theo systems, không giao dịch theo cảm tính, không để cho cảm xúc xen vào các quyết định giao dịch."
-]
+];
+
+export const faqs = [
+  {
+    title: "1.Tín hiệu phân kì là gì ?",
+    content: [
+      "Tức là giá thì tăng mà RSI thì tạo đỉnh thấp dần ( giải thích điều này là khi mà giá tăng mạnh nhưng mà lúc này RSI đi xuống tạo đỉnh thấp dần, chứng tỏ lực mua đã suy yếu, lực bán đã tham gia vào nhiều, thì cần lập tức bỏ chạy.",
+      "Lưu ý: Có dấu hiệu phân kì cũng chưa chắc là giá đã giảm, mà giá nó có thể giảm xíu xong nó tăng tiếp rất mạnh. Việc của mình là thấy có dấu hiệu phân kì thì mình biết rằng giá có khả năng điều chỉnh, còn điều chỉnh xong nó có tăng tiếp hay không thì câu chuyện đó tính sau .Nếu thấy RSI điều chỉnh xong và đo fibo thấy vẫn giữ giá tốt thì lại tiếp tục tham gia mua vào"
+    ]
+  },
+  {
+    title:
+      "2.Khi nhìn thấy dấu hiệu phân kì giảm thì có nên bay vào Short liền hay không?",
+    content: [
+      "Không nên, nếu có ý định Short thì cần phải quan sát xu hướng của khung lớn hơn, và xem lực mua ở khung nhỏ hơn. Khi ấy có thể treo lệnh chờ để chờ đợi cơ hội."
+    ]
+  },
+  {
+    title:
+      "3.RSI ở các khung thời gian khác nhau thì cho các tín hiệu chỉ báo khác nhau như vậy có phải đồng thuận không ?",
+    content: [
+      "Áp dụng lý thuyết của sóng Eliot thì trong con sóng tăng chính sẽ có những con sóng nhỏ, trong con sóng nhỏ sẽ có những con sóng nhỏ hơn, khung thời gian càng lớn thì cho thấy xu hướng chính càng rõ rệt.",
+      "Trong con sóng tăng cũng có thời điểm nó sẽ dừng lại điều chỉnh, sideway rồi mới tăng tiếp. Khi này nếu nhìn ở khung nhỏ hơn thì sẽ khá nhiễu loạn. Việc của mình là khi view khung nhỏ thấy nhiễu loạn thì sẽ bỏ qua và chuyển sang khung lớn hơn, có tín hiệu rõ ràng hơn để follow theo."
+    ]
+  },
+  {
+    title:
+      "4.Khi nào thì nên đặt lệnh dừng lỗ, khi nào thì không cần đặt lệnh dừng lỗ ?",
+    content: [
+      "Đặt lệnh dừng lỗ cần tuân thủ tuyệt đối trong một số trường hợp như sau: giao dịch ở khung H4 trở xuống, giao dịch phái sinh.",
+      "Lệnh dừng lỗ không cần thiết phải đặt nếu đầu tư trong khung thời gian 1D trở lên, thậm chí có thể áp dụng chiến lược mua DCA khi giá tiếp tục giảm, nhưng phải hiểu thật sự sâu về đầu tư, cũng như vốn phải thật sự sẵn sàng cho dài hạn."
+    ]
+  },
+  {
+    title: "5.Có nên giao dịch future hay không?",
+    content: [
+      "Đối với những người mới chưa có kĩ năng master về trading thì lời khuyên chân thành là không nên, làm sao mà giao dịch spot có được lợi nhuận ổn định trong một thời gian rất là dài rồi thì khi ấy mới cân nhắc tới việc đánh future."
+    ]
+  },
+  {
+    title: "6.Có nên đánh Altcoin hay không?",
+    content: [
+      "Altcoin nó khá là rủi ro nên nếu tham gia thì chỉ dành 1 số vốn nhỏ, và phải tìm hiểu thật kĩ lưỡng về dự án đó trước khi đưa ra quyết định đầu tư, ngoài ra cũng cần xác định xem chu kì thị trường đang ở giai đoạn nào."
+    ]
+  },
+  {
+    title:
+      "7.Khi nhìn thấy dấu hiệu khung H1 đã đi xa và suy yếu thì có vội vàng thoát lệnh mua hay không?",
+    content: [
+      "Không nên, cần quan sát khung giao dịch xem sóng đã đi xa hay chưa, hiện đang là sóng mấy, quan sát thêm các dấu hiệu Price action, fibo có giữ cấu trúc tăng tiếp hay không, cách tốt nhất là nên quản lý lệnh theo systems và thoát lệnh khi đã đạt ra target trước đó.."
+    ]
+  }
+];
