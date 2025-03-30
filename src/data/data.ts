90605f374206006d343a406ebc77ccd71cc3017e
HuyDoan <doanvanhuy268@gmail.com>
2025-03-30 15:25:03 +0700

    fe

--- src/data/data.ts
+++ src/data/data.ts
@@ -82,7 +82,7 @@ export const allData: YearlyData = {
   "2025": [
     { name: "01/2025", revenue: -1018 },
     { name: "02/2025", revenue: -129 },
-    { name: "03/2025", revenue: -3 }
+    { name: "03/2025", revenue: -48 }
   ]
 };
 
@@ -178,48 +178,7 @@ export const faqs = [
       "Lưu ý: Có dấu hiệu phân kì cũng chưa chắc là giá đã giảm, mà giá nó có thể giảm xíu xong nó tăng tiếp rất mạnh. Việc của mình là thấy có dấu hiệu phân kì thì mình biết rằng giá có khả năng điều chỉnh, còn điều chỉnh xong nó có tăng tiếp hay không thì câu chuyện đó tính sau .Nếu thấy RSI điều chỉnh xong và đo fibo thấy vẫn giữ giá tốt thì lại tiếp tục tham gia mua vào"
     ]
   },
-  {
-    title:
-      "2.Khi nhìn thấy dấu hiệu phân kì giảm thì có nên bay vào Short liền hay không?",
-    content: [
-      "Không nên, nếu có ý định Short thì cần phải quan sát xu hướng của khung lớn hơn, và xem lực mua ở khung nhỏ hơn. Khi ấy có thể treo lệnh chờ để chờ đợi cơ hội."
-    ]
-  },
-  {
-    title:
-      "3.RSI ở các khung thời gian khác nhau thì cho các tín hiệu chỉ báo khác nhau như vậy có phải đồng thuận không ?",
-    content: [
-      "Áp dụng lý thuyết của sóng Eliot thì trong con sóng tăng chính sẽ có những con sóng nhỏ, trong con sóng nhỏ sẽ có những con sóng nhỏ hơn, khung thời gian càng lớn thì cho thấy xu hướng chính càng rõ rệt.",
-      "Trong con sóng tăng cũng có thời điểm nó sẽ dừng lại điều chỉnh, sideway rồi mới tăng tiếp. Khi này nếu nhìn ở khung nhỏ hơn thì sẽ khá nhiễu loạn. Việc của mình là khi view khung nhỏ thấy nhiễu loạn thì sẽ bỏ qua và chuyển sang khung lớn hơn, có tín hiệu rõ ràng hơn để follow theo."
-    ]
-  },
-  {
-    title:
-      "4.Khi nào thì nên đặt lệnh dừng lỗ, khi nào thì không cần đặt lệnh dừng lỗ ?",
-    content: [
-      "Đặt lệnh dừng lỗ cần tuân thủ tuyệt đối trong một số trường hợp như sau: giao dịch ở khung H4 trở xuống, giao dịch phái sinh.",
-      "Lệnh dừng lỗ không cần thiết phải đặt nếu đầu tư trong khung thời gian 1D trở lên, thậm chí có thể áp dụng chiến lược mua DCA khi giá tiếp tục giảm, nhưng phải hiểu thật sự sâu về đầu tư, cũng như vốn phải thật sự sẵn sàng cho dài hạn."
-    ]
-  },
-  {
-    title: "5.Có nên giao dịch future hay không?",
-    content: [
-      "Đối với những người mới chưa có kĩ năng master về trading thì lời khuyên chân thành là không nên, làm sao mà giao dịch spot có được lợi nhuận ổn định trong một thời gian rất là dài rồi thì khi ấy mới cân nhắc tới việc đánh future."
-    ]
-  },
-  {
-    title: "6.Có nên đánh Altcoin hay không?",
-    content: [
-      "Altcoin nó khá là rủi ro nên nếu tham gia thì chỉ dành 1 số vốn nhỏ, và phải tìm hiểu thật kĩ lưỡng về dự án đó trước khi đưa ra quyết định đầu tư, ngoài ra cũng cần xác định xem chu kì thị trường đang ở giai đoạn nào."
-    ]
-  },
-  {
-    title:
-      "7.Khi nhìn thấy dấu hiệu khung H1 đã đi xa và suy yếu thì có vội vàng thoát lệnh mua hay không?",
-    content: [
-      "Không nên, cần quan sát khung giao dịch xem sóng đã đi xa hay chưa, hiện đang là sóng mấy, quan sát thêm các dấu hiệu Price action, fibo có giữ cấu trúc tăng tiếp hay không, cách tốt nhất là nên quản lý lệnh theo systems và thoát lệnh khi đã đạt ra target trước đó.."
-    ]
-  }
+ 
 ];
 
 export const quintessenceRsi = [
