0b959bc506c5033def064f49fd2a1811ae7c4410
HuyDoan <doanvanhuy268@gmail.com>
2025-03-30 16:00:34 +0700

    fd

--- src/data/data.ts
+++ src/data/data.ts
@@ -172,10 +172,15 @@ export const importantNotes = [
 
 export const faqs = [
   {
-    title: "1.Tín hiệu phân kì là gì ?",
+    title: "1. Tín hiệu phân kì là gì ?",
     content: [
-      "Tức là giá thì tăng mà RSI thì tạo đỉnh thấp dần ( giải thích điều này là khi mà giá tăng mạnh nhưng mà lúc này RSI đi xuống tạo đỉnh thấp dần, chứng tỏ lực mua đã suy yếu, lực bán đã tham gia vào nhiều, thì cần lập tức bỏ chạy.",
-      "Lưu ý: Có dấu hiệu phân kì cũng chưa chắc là giá đã giảm, mà giá nó có thể giảm xíu xong nó tăng tiếp rất mạnh. Việc của mình là thấy có dấu hiệu phân kì thì mình biết rằng giá có khả năng điều chỉnh, còn điều chỉnh xong nó có tăng tiếp hay không thì câu chuyện đó tính sau .Nếu thấy RSI điều chỉnh xong và đo fibo thấy vẫn giữ giá tốt thì lại tiếp tục tham gia mua vào"
+      "Tức là giá thì tăng mà RSI thì tạo đỉnh thấp dần ( giải thích điều này là khi mà giá tăng mạnh nhưng mà lúc này RSI đi xuống tạo đỉnh thấp dần, chứng tỏ lực mua đã suy yếu, lực bán đã tham gia vào nhiều, thì cần lập tức bỏ chạy.\n\nLưu ý:\n Có dấu hiệu phân kì cũng chưa chắc là giá đã giảm, mà giá nó có thể giảm xíu xong nó tăng tiếp rất mạnh.\n Việc của mình là thấy có dấu hiệu phân kì thì mình biết rằng giá có khả năng điều chỉnh, còn điều chỉnh xong nó có tăng tiếp hay không thì câu chuyện đó tính sau.\nNếu thấy RSI điều chỉnh xong và đo fibo thấy vẫn giữ giá tốt thì lại tiếp tục tham gia mua vào.",
+    ]
+  },
+  {
+    title: "2. Vùng tích luỹ lực mua, vùng tích luỹ lực bán là gì, làm thế nào để xác định được nó ?",
+    content: [
+      "Vùng tích luỹ lực mua:\nHiểu là vùng giá mà lực bán đã suy yếu và có sự tham gia vào của lực mua.\nVà đây là vùng giá có khả năng đảo chiều từ giảm thành tăng.\n\nVùng tích luỹ lực bán:\nHiểu là vùng giá mà lực mua đã suy yếu, lực bán bắt đầu mạnh lên...\nVà đây là vùng giá có khả năng đảo chiều từ tăng thành giảm.\n\n==> Để xác định vùng giá này thì cần áp dụng rất nhiều kiến thức:\n\nRSI, Lý thuyết Dow, Sóng Elliott, Phân tích đa khung thời gian..."
     ]
   },
  
