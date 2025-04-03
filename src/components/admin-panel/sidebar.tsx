dcb83df920cdb0af71718f798623f9af6f68d171
huy.nguyen <huy.nguyen8@sotatek.com>
2025-04-03 10:33:23 +0700

    fd

--- src/components/admin-panel/sidebar.tsx
+++ src/components/admin-panel/sidebar.tsx
@@ -5,8 +5,7 @@ import { Button } from "@/components/ui/button";
 import { useSidebar } from "@/hooks/use-sidebar";
 import { useStore } from "@/hooks/use-store";
 import { cn } from "@/lib/utils";
-import { PanelsTopLeft } from "lucide-react";
-import Link from "next/link";
+import { TentTree } from "lucide-react";
 
 export function Sidebar() {
   const sidebar = useStore(useSidebar, (x) => x);
@@ -35,7 +34,7 @@ export function Sidebar() {
           asChild
         >
           <div className="flex items-center gap-2">
-            <PanelsTopLeft className="w-6 h-6 mr-1" />
+            <TentTree className="w-6 h-6 mr-1" />
             <h1
               className={cn(
                 "font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
