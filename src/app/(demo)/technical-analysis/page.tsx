21a585416d1de6b85013fa98506d74746401f946
HuyDoan <doanvanhuy268@gmail.com>
2025-04-13 15:48:28 +0700

    df

--- src/app/(demo)/technical-analysis/page.tsx
+++ src/app/(demo)/technical-analysis/page.tsx
@@ -9,7 +9,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Checkbox } from "@/components/ui/checkbox"
-import { conditionForEnteringATrade, quintessenceRsi } from "@/data/data";
+import { quintessenceRsi, supportAndResistance } from "@/data/data";
 
 export default function QuintessenceOfRsiPage() {
   const sidebar = useStore(useSidebar, (x) => x);
@@ -29,17 +29,35 @@ export default function QuintessenceOfRsiPage() {
       <Card className="max-h-[67.5vh] overflow-auto shadow-lg border border-black-200 dark:border-black-700">
         <CardContent className="p-6 space-y-4">
           <div className="font-bold text-lg text-black-800 dark:text-white">
-          Tinh hoa trong bộ công cụ chỉ báo RSI:
+            Tinh hoa trong bộ công cụ chỉ báo RSI:
           </div>
           <div className="space-y-2">
             {quintessenceRsi.map((item, idx) => (
               <div key={idx} className="flex items-start space-x-2 ">
-                <Checkbox/>
+                <Checkbox />
                 <Label className="text-black-700 font-semibold dark:text-white">
                   {item}
                 </Label>
               </div>
             ))}
+          </div>
+        </CardContent>
+      </Card>
+      <Card className="max-h-[67.5vh] overflow-auto shadow-lg border border-black-200 dark:border-black-700">
+        <CardContent className="p-6 space-y-4">
+          <div className="font-bold text-lg text-black-800 dark:text-white">
+            Tinh hoa của hỗ trợ & kháng cự:
+          </div>
+          <div className="space-y-2">
+            {supportAndResistance
+              .map((item, idx) => (
+                <div key={idx} className="flex items-start space-x-2 ">
+                  <Checkbox />
+                  <Label className="text-black-700 font-semibold dark:text-white">
+                    {item}
+                  </Label>
+                </div>
+              ))}
           </div>
         </CardContent>
       </Card>
