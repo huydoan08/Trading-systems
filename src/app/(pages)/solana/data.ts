59eb310ccbabcd3b0d7de19f4ff03bb2c8c66050
huy.doan <86400709+huydoan08@users.noreply.github.com>
2025-09-10 23:01:11 +0700

    update so

---
+++ src/app/(pages)/solana/data.ts
@@ -0,0 +1,11 @@
+export const supportAndResistance = [
+  "Chia vốn ra nhiều lần mua",
+  "Khi sóng chạy xa thì bán dần ra để chốt lời"
+];
+
+export const quintessenceRsi = [
+  "Khung 1D: RSI về dưới vùng 30 => Vùng mua lướt trung hạn.",
+  "Khung 3D: Con sóng điều kiện là RSI 3D vượt ra ngoài vùng 80 => Chờ con sóng điều chỉnh khi mà RSI, đường trung bình nhanh, đường trung bình chậm bắt đầu chụm lại là vùng mua và lướt trung hạn.",
+  "Khung 1W: RSI rơi xuống dưới vùng 30 => Vùng tích luỹ dài hạn nên chỉ mua và nắm giữ.",
+  "Khung 2W: RSI rơi xuống dưới vùng 30 => Vùng tích luỹ dài hạn nên chỉ mua và nắm giữ."
+];
