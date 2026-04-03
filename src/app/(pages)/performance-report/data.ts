08506ac0c79b0c69ee4048a073a760e274b0ce04
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-04-03 20:54:18 +0700

    1

--- src/app/(pages)/performance-report/data.ts
+++ src/app/(pages)/performance-report/data.ts
@@ -28,7 +28,7 @@ export const allData: YearlyData = {
   "2026": [
     { name: "01/2026", revenue: -11 },
     { name: "02/2026", revenue: -14 },
-    { name: "03/2026", revenue: 11 },
+    { name: "03/2026", revenue: -1 },
     { name: "04/2026", revenue: 0 },
     { name: "05/2026", revenue: 0 },
     { name: "06/2026", revenue: 0 },
