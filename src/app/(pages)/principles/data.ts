09cdb0196de4536e83e68fc7082d3df9b0a7be8d
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-06-06 10:09:09 +0700

    a

--- src/app/(pages)/principles/data.ts
+++ src/app/(pages)/principles/data.ts
@@ -2,5 +2,6 @@ export const quintessenceRsi = [
   "Chỉ giao dịch SPOT BTC",
   "Đánh theo RSI khung 1D",
   "Gom hàng và thoát hàng từng phần",
+  "Tổng khối lượng vào lệnh tối đa 1000 USDT",
   "Chỉ ngó chart 1 đến 2 lần mỗi ngày",
 ];
