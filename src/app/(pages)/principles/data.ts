55dda92fc8b578aafadacffe0c921b18f4ff49c8
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-06-27 15:37:08 +0700

    1

--- src/app/(pages)/principles/data.ts
+++ src/app/(pages)/principles/data.ts
@@ -1,16 +1,17 @@
 export const quintessenceRsi = [
-  "Giao dịch theo khung H4.",
-  "RSI các khung 1D, H4, H1 đều phải đồng pha với nhau.",
-  "Chờ xác nhận của sóng hồi để tối ưu điểm vào lệnh đẹp.",
+  "Chỉ tập trung vào BTC và ETH.",
+  "Chỉ giao dịch chiều lên, không có suy nghĩ Short trong đầu",
+  "Giao dịch theo RSI khung H4 hoặc khung 1D",
+  "Phải chờ xác nhận của sóng hồi để tối ưu điểm vào lệnh đẹp.",
   "Có dấu hiệu xác nhận của Price Action là một điểm cộng.",
   "Xác định trendline giá phá vỡ xu hướng trước đó.",
-  "Tối ưu điểm vào lệnh đẹp ở khung thời gian nhỏ.",
-  "Stoploss là bắt buộc, volumn vào lệnh là 1000$ cho mỗi lệnh (SL ~ 25$).",
+  "Tối ưu điểm vào lệnh đẹp ở khung thời gian nhỏ hơn.",
+  "Stoploss là bắt buộc đối với khung H4, volumn vào lệnh là 1000$ cho mỗi lệnh (SL ~ 25$).",
   "Xác định mức thưởng nếu chiến thắng lệnh đó đáng để giao dịch hay là không.",
   "Không mạo hiểm số tiền lớn với bất kỳ một lệnh giao dịch nào.",
   "Không bao giờ đoán đỉnh hoặc đoán đáy mà phải chờ giá quay lại retest.",
   "Khi vào lệnh, xác định rõ là có thể thua lỗ, không chắc chắn một điều gì cả.",
-  "Ghi chép nhật ký cho mỗi lệnh giao dịch để hoàn thiện hơn"
+  "Ghi chép nhật ký cho mỗi lệnh giao dịch để hoàn thiện hơn."
 ];
 export const ManageStrategy = [
   "Quản lý lệnh theo cấu trúc sóng, follow chiến lược giao dịch một cách tuyệt đối.",
@@ -18,5 +19,5 @@ export const ManageStrategy = [
   "Chỉ nhồi thêm lệnh khi giá đi khá xa và lệnh 1 có lợi nhuận, dời SL quản lý lệnh khi giá đã tạo đỉnh đáy.",
   "Tiền kiếm được trong lúc chờ đợi, chứ không phải là LIÊN TỤC VÀO LỆNH.",
   "Giao dịch theo xu hướng – Cắt vị thế lỗ nhanh chóng – Duy trì vị thế lời – Đừng đóng vị thế khi đang có khoản lợi nhuận lớn.",
-  "→ Thực hành nhiều, quan sát nhiều, tối ưu nhiều… bạn sẽ biết cách follow theo con sóng."
+  "Thực hành nhiều, quan sát nhiều, tối ưu nhiều… bạn sẽ biết cách follow theo con sóng."
 ];
