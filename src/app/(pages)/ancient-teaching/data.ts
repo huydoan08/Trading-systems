d649eb5dd6450792c4f3cb63ebb8cd67d1a07fa6
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-07-14 23:06:28 +0700

    1

--- src/app/(pages)/ancient-teaching/data.ts
+++ src/app/(pages)/ancient-teaching/data.ts
@@ -2,155 +2,232 @@ export const ancientTeaching = [
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-09.webp"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-17.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-06.webp"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-06.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-12.jpg"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-09.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-04.webp"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-12.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-66.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/111.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-64.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/112.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-56.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/113.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-55.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/115.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-51.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/116.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-49.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/117.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-47.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/127.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-45.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/131.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-44.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/132.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-42.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/133.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-41.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/151.jpg"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-38.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-04.webp"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-30.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-66.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-27.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-64.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-20.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-56.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-17.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-55.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-16.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-51.png"
+    }
   },
   {
     title: "",
     content: {
-        type: "image" as const,
-        value: "/personal-growth/growth-14.png"
-      }
+      type: "image" as const,
+      value: "/personal-growth/growth-49.png"
+    }
   },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-47.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-45.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-44.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-42.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-41.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-38.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-30.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-27.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-20.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-16.png"
+    }
+  },
+  {
+    title: "",
+    content: {
+      type: "image" as const,
+      value: "/personal-growth/growth-14.png"
+    }
+  }
 ];
