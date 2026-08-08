f99320975c1f301c741c26a1f4cea0eba807373f
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-08-08 16:20:42 +0700

    1

--- src/app/(pages)/HtmlCss/data.ts
+++ src/app/(pages)/HtmlCss/data.ts
@@ -6,5 +6,9 @@ export const HtmlCss = [
     content: [
       "selector:has(condition) sẽ chọn phần tử cha nếu phần tử con thỏa điều kiện \n\nTrong đó selector là phần tử cha, condition là điều kiện của phần tử con\n\n Ví dụ: div:has(> p) sẽ chọn tất cả các thẻ div có chứa thẻ p trực tiếp bên trong nó \n\n Hoặc div:has(> p.text) sẽ chọn tất cả các thẻ div có chứa thẻ p với class là text trực tiếp bên trong nó",
     ]
+  },
+  {
+    title: "Dùng pseudo Class :global",
+    content: ["Dùng pseudo Class :global thường gặp trong CSS Modules (đặc biệt khi dùng React/Next.js), dùng để nói rằng selector bên trong không bị CSS Modules tự động hash/scoped, mà áp dụng như CSS thông thường."]
   }
 ];
