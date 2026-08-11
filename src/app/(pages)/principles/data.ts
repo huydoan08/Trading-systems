f4346e0e42e2fe35bede02d7b7849db5606a16c2
huy.doan <86400709+huydoan08@users.noreply.github.com>
2026-08-11 17:32:17 +0700

    1

--- src/app/(pages)/principles/data.ts
+++ src/app/(pages)/principles/data.ts
@@ -6,10 +6,10 @@ export const rules = [
   "Ghi chép nhật ký, rút ra bài học sau mỗi một lệnh để ngày một hoàn thiện hơn.",
 ];
 export const EntryStrategy = [
-  { label: "RSI của khung giao dịch đồng pha với khung lớn hơn", score: 30 },
-  { label: "RSI của khung giao dịch nhỏ hơn đã giảm hết hành trình", score: 30 },
-  { label: "Giá đã quay lại retest xong.", score: 20 },
-  { label: "Có sự xác nhận của price action.", score: 20 },
+  { label: "RSI của khung giao dịch đồng pha với khung lớn hơn", score: 25 },
+  { label: "RSI của khung giao dịch nhỏ hơn đã giảm hết hành trình", score: 25 },
+  { label: "Giá của khung giao dịch nhỏ hơn đã quay lại retest xong và không phá qua đáy cũ.", score: 25 },
+  { label: "Có sự xác nhận của price action.", score: 25 },
 ];
 
 export const ManageStrategy = [
